26c975cddccfc0b2e6be7395cebf25b8908927fa
Alexander Urizar <urizaraj@gmail.com>
2018-02-07 21:25:20 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,9 +1,7 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
   string = ""
-  [1..colors.length].each do |i|
-    j = i - 1
-    color = colors[j]
+  colors.each do |color|
     string = string + "#{color[0].upcase()}: #{color}, "
   end
   puts string[0..-3]
