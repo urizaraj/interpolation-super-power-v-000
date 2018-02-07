ef5c57a5be228791ffd72e59c8bfc3f6c95a8f7f
Alexander Urizar <urizaraj@gmail.com>
2018-02-07 21:13:34 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow(array)
   array.each do |color|
-    print "#{color[0].upcase()}: #{color},"
+    print "#{color[0].upcase()}: #{color}, "
   end
 end
